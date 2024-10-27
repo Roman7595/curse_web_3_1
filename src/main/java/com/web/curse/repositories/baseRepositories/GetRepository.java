284cea019e4d1b2203964d7f263ede12e8f98d2c
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2024-10-27 03:36:08 +0300

    add base repos

---
+++ src/main/java/com/web/curse/repositories/baseRepositories/GetRepository.java
@@ -0,0 +1,9 @@
+package com.web.curse.repositories.baseRepositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GetRepository<T>{
+    public Optional<T> findById(int id, Class<T> type);
+    public List<T> findAll(Class<T> type);
+}
---
+++ src/main/java/com/web/curse/repositories/baseRepositories/GetRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.web.curse.repositories.baseRepositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public abstract class GetRepositoryImpl<T> implements GetRepository<T>{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Transactional
+    public Optional<T> findById(int id, Class<T> entityClass){
+        return Optional.of(entityManager.find(entityClass, id));
+    }
+
+    @Transactional
+    public List<T> findAll(Class<T> entityClass){
+        return entityManager.createQuery("Select t from " + entityClass.getName() + " t").getResultList();
+
+    }
+
+}
