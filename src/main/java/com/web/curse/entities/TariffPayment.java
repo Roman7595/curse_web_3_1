c952991beba3297683a10871a68c965ce36b665f
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2024-11-06 00:55:54 +0300

    fix tariff payment logic

--- src/main/java/com/web/curse/entities/TariffPayment.java
+++ src/main/java/com/web/curse/entities/TariffPayment.java
@@ -8,27 +8,19 @@ import java.util.Date;
 @Table(name = "tariff_payments")
 public class TariffPayment extends  BaseEntity{
     private double waterUsage;
-    private double waterSum;
     private double singleElectricalUsage;
-    private double singleElectricalSum;
     private double doubleElectricalDayUsage;
-    private double doubleElectricalDaySum;
     private double doubleElectricalNightUsage;
-    private double doubleElectricalNightSum;
     private Date paymentDate;
     private Tariff tariff;
     private Land land;
 
-    public TariffPayment(double waterUsage, double waterSum, double singleElectricalUsage, double singleElectricalSum, double doubleElectricalDayUsage, double doubleElectricalDaySum, double doubleElectricalNightUsage, double doubleElectricalNightSum,Tariff tariff,Land land) {
+    public TariffPayment(double waterUsage, double singleElectricalUsage, double doubleElectricalDayUsage, double doubleElectricalNightUsage, Tariff tariff, Land land) {
         this.waterUsage = waterUsage;
-        this.waterSum = waterSum;
         this.singleElectricalUsage = singleElectricalUsage;
-        this.singleElectricalSum = singleElectricalSum;
         this.doubleElectricalDayUsage = doubleElectricalDayUsage;
-        this.doubleElectricalDaySum = doubleElectricalDaySum;
         this.doubleElectricalNightUsage = doubleElectricalNightUsage;
-        this.doubleElectricalNightSum = doubleElectricalNightSum;
-        this.paymentDate = new Date();
+        this.paymentDate = null;
         this.tariff = tariff;
         this.land = land;
     }
@@ -39,40 +31,24 @@ public class TariffPayment extends  BaseEntity{
         return waterUsage;
     }
 
-    @Column(name = "water_sum",nullable = false)
-    public double getWaterSum() {
-        return waterSum;
-    }
 
     @Column(name = "single_electrical_usage",nullable = false)
     public double getSingleElectricalUsage() {
         return singleElectricalUsage;
     }
 
-    @Column(name = "single_electrical_sum",nullable = false)
-    public double getSingleElectricalSum() {
-        return singleElectricalSum;
-    }
 
     @Column(name = "double_electrical_day_usage",nullable = false)
     public double getDoubleElectricalDayUsage() {
         return doubleElectricalDayUsage;
     }
 
-    @Column(name = "double_electrical_day_sum",nullable = false)
-    public double getDoubleElectricalDaySum() {
-        return doubleElectricalDaySum;
-    }
 
     @Column(name = "double_electrical_night_usage",nullable = false)
     public double getDoubleElectricalNightUsage() {
         return doubleElectricalNightUsage;
     }
 
-    @Column(name = "double_electrical_night_sum",nullable = false)
-    public double getDoubleElectricalNightSum() {
-        return doubleElectricalNightSum;
-    }
 
     @Column(name = "payment_date")
     public Date getPaymentDate() {
@@ -103,33 +79,21 @@ public class TariffPayment extends  BaseEntity{
         this.waterUsage = waterUsage;
     }
 
-    public void setWaterSum(double waterSum) {
-        this.waterSum = waterSum;
-    }
 
     public void setSingleElectricalUsage(double singleElectricalUsage) {
         this.singleElectricalUsage = singleElectricalUsage;
     }
 
-    public void setSingleElectricalSum(double singleElectricalSum) {
-        this.singleElectricalSum = singleElectricalSum;
-    }
 
     public void setDoubleElectricalDayUsage(double doubleElectricalDayUsage) {
         this.doubleElectricalDayUsage = doubleElectricalDayUsage;
     }
 
-    public void setDoubleElectricalDaySum(double doubleElectricalDaySum) {
-        this.doubleElectricalDaySum = doubleElectricalDaySum;
-    }
 
     public void setDoubleElectricalNightUsage(double doubleElectricalNightUsage) {
         this.doubleElectricalNightUsage = doubleElectricalNightUsage;
     }
 
-    public void setDoubleElectricalNightSum(double doubleElectricalNightSum) {
-        this.doubleElectricalNightSum = doubleElectricalNightSum;
-    }
 
     protected void setPaymentDate(Date paymentDate){
         this.paymentDate = paymentDate;
